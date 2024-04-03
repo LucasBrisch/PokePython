14bd0a679ee6b7211480033b9f26bae487b77c73
Lucas-BZan <lucasbrisch@gmail.com>
2024-04-03 17:34:10 -0300

    delay print aplicado!

--- escolha.py
+++ escolha.py
@@ -9,37 +9,56 @@ def delay_print(s):
         time.sleep(0.05)
         
 def escolha_inicial():
-    global player_name, starter_pokemon
+    global player_name, starter_pokemon, pokemon_1
     print()
-    player_name = str(input('''Olá, meu nome é Professor Carvalho, bem-vindo ao mundo dos Pokémons! Este mundo é habitado 
-    por criaturas chamadas Pokémons! Para algumas pessoas, os Pokémons são animais de estimação. Outros os usam para lutar. 
-    Eu mesmo... Estudo Pokémons como profissão.\n\nAgora me diga, qual é o seu nome? ''')).strip().lower().title()
-    starter_pokemon = str(input(f'''É um prazer! {player_name}, agora me diga, qual vai ser o seu pokemon inicial?
-    \n Squirtle(água), Charmander(fogo), Bulbasaur(grama) ou Pikachu(elétrico)? ''')).strip().lower()
+    delay_print('''Olá, meu nome é Professor Carvalho, bem-vindo ao mundo dos Pokémons! Este mundo é habitado 
+por criaturas chamadas Pokémons! Para algumas pessoas, os Pokémons são animais de estimação. Outros os usam para lutar. 
+Eu mesmo... Estudo Pokémons como profissão.\n\nAgora me diga, qual é o seu nome?\n''')
+    player_name = str(input()) 
+    
+    delay_print(f'''É um prazer! {player_name}, agora me diga, qual vai ser o seu pokemon inicial?\n
+Squirtle(água), Charmander(fogo), Bulbasaur(grama) ou Pikachu(elétrico)? \n''')
+    starter_pokemon = str(input())
+    
     while starter_pokemon not in ["squirtle","água","squirtle(água)","charmander","fogo","charmander(fogo)",
     "bulbasaur","grama","bulbasaur(grama)","pikachu","elétrico","pikachu(elétrico)"]:
         print()
-        print("este não é um pokémon inicial válido, por favor, escolha um dos pokémons iniciais disponíveis!\n")
-        starter_pokemon = input("Então, vai ser o Squirtle(água), Charmander(fogo), Bulbasaur(grama) ou Pikachu(eletrico)? ").strip().lower()
+        delay_print("este não é um pokémon inicial válido, por favor, escolha um dos pokémons iniciais disponíveis!\n")
+        ("Então, vai ser o Squirtle(água), Charmander(fogo), Bulbasaur(grama) ou Pikachu(eletrico)? ").strip().lower()
+        starter_pokemon = str(input())
     if starter_pokemon in ["squirtle","água","squirtle(água)", 'agua']:
-        print("Você escolheu o Squirtle; ótima escolha!\n")
+        delay_print("Você escolheu o Squirtle; ótima escolha!\n")
         pokemon_1 = 'Squirtle'
     elif starter_pokemon in ["charmander","fogo","charmander(fogo)"]:
-        print("Você escolheu o Charmander; ótima escolha!\n")
+        delay_print("Você escolheu o Charmander; ótima escolha!\n")
         pokemon_1 = 'Charmander'
     elif starter_pokemon in ["bulbasaur","grama","bulbasaur(grama)"]:
-        print("Você escolheu o Bulbasaur; ótima escolha!\n")
+        delay_print("Você escolheu o Bulbasaur; ótima escolha!\n")
         pokemon_1 = 'Bulbasaur'
     elif starter_pokemon in ["pikachu","elétrico","pikachu(elétrico)", 'eletrico']:
-        print("Ah, o bom e velho Pikachu, ótima escolha!\n")
+        delay_print("Ah, o bom e velho Pikachu, ótima escolha!\n")
         pokemon_1 = 'Pikachu'
-    escolha = input(f"Agora me diga, você gostaria de dar um apelido ao seu {pokemon_1}? (sim/nao) ").strip().lower()
-    if escolha == "sim":
-        pokemon_1 = str(input("Qual será o apelido do seu Pokémon? ")).strip().lower().title()
-        print(f"Ótimo! A partir de agora, {pokemon_1} será o nome do seu Pokémon inicial!")
-    elif escolha == "nao":
-        print(f"Entendido! {pokemon_1} será o nome do seu Pokémon inicial!")
-        print('Agora que você escolheu seu Pokémon inicial, você está pronto para começar sua jornada! Boa sorte!\n')
+    delay_print(f'''Agora me diga, você gostaria de dar um apelido ao seu {pokemon_1}? 
+[1] sim
+[2] nao) ''')
+    escolha = input()
+    
+    while escolha not in ["1","2"]:
+        print()
+        delay_print("Por favor, escolha uma opção válida!\n")
+        delay_print(f'''Agora me diga, você gostaria de dar um apelido ao seu {pokemon_1}? 
+[1] sim
+[2] nao) ''').strip().lower() 
+        escolha = input ()
+    if escolha == "1":
+        delay_print("Qual será o apelido do seu Pokémon? ")
+        pokemon_1 = input()
+        delay_print(f"Ótimo! A partir de agora, {pokemon_1} será o nome do seu Pokémon inicial!")
+    elif escolha == "2":
+        delay_print(f"Entendido! {pokemon_1} será o nome do seu Pokémon inicial!")
+
+delay_print('Agora que você escolheu seu Pokémon inicial, você está pronto para começar sua jornada! Boa sorte!\n')
+    
         
 if __name__ == "__main__":
     escolha_inicial()
